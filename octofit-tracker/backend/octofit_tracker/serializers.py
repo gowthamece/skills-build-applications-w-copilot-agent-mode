e9304fcafbaff647db7b07bf7a381726c786780e
Gowtham K <gowthamk91@outlook.com>
2025-04-08 18:57:48 +0000

    Initialize Django project structure with core files and models

---
+++ octofit-tracker/backend/octofit_tracker/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+from .models import User, Team, Activity, Leaderboard, Workout
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+class TeamSerializer(serializers.ModelSerializer):
+    members = UserSerializer(many=True)
+
+    class Meta:
+        model = Team
+        fields = '__all__'
+
+class ActivitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Activity
+        fields = '__all__'
+
+class LeaderboardSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
+    class Meta:
+        model = Leaderboard
+        fields = '__all__'
+
+class WorkoutSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Workout
+        fields = '__all__'
